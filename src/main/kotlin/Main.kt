ff2cabcf86f7b04ac994383a9a414d0bf9578c68
Anastasia Iozefi <aiozefi@gmail.com>
2024-01-11 22:16:46 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val likesCount = 61 // Rоличество лайков
+
+    val formattedText = formatLikesText(likesCount)
+    println(formattedText)
+}
+
+fun formatLikesText(likesCount: Int): String {
+    val lastDigit = likesCount % 10
+    val lastTwoDigits = likesCount % 100
+
+    return when {
+        lastDigit == 1 && lastTwoDigits != 11 -> "Понравилось $likesCount человеку"
+        (lastDigit in 2..4 && (lastTwoDigits < 10 || lastTwoDigits > 20)) -> "Понравилось $likesCount людям"
+        else -> "Понравилось $likesCount людям"
+    }
+}
